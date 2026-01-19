cbaaf03902322731820e0b03d82aca3a178b36d3
swhitehair <swhitehair006@gmail.com>
2026-01-19 17:37:06 -0500

    8 way movement

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed :int = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
